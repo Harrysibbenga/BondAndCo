5945f0f06bf62083724cd60b11e76ef90fa54031
hs <sibbengaharry@gmail.com>
2021-05-26 13:39:09 +0100

    Built the basic layout for each service page

---
+++ components/Service/head.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <v-img :src="require('../../assets/images/' + head.url)" :min-height="800">
+      <v-row align="end" justify="end" class="row-height">
+        <div class="black title-container pa-5">
+          <h1 class="text-h3 text-md-h1 white--text text-right">
+            {{ head.title }}
+          </h1>
+          <v-row align="end" justify="end" class="py-5 pt-10 pr-3">
+            <hr />
+            <hr />
+            <hr />
+          </v-row>
+        </div>
+      </v-row>
+    </v-img>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    head: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
+
+<style>
+.title-container {
+  width: 300px;
+}
+
+.row-height {
+  height: 700px;
+}
+
+hr {
+  border: none;
+  border-top: 10px solid #364799;
+  height: 5px;
+  margin: 0 5px 0 5px;
+  width: 20%;
+}
+
+@media (min-width: 960px) {
+  .title-container {
+    width: 600px;
+  }
+}
+</style>
---
+++ components/Service/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <service-head :head="service.head"></service-head>
+    <service-body :body="service.body"></service-body>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    service: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
